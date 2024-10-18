2b75631615c420d401473de545dcf6130e5a10e1
dntelisa <elisabzh10@gmail.com>
2024-10-18 10:39:31 +0200

    Update StringUtils.java

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,24 @@
 package fr.istic.vv;
+import java.util.Stack;
 
 public class StringUtils {
 
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
+            } 
+            else if (ch == '}' || ch == ']' || ch == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                return (top == '{' && ch == '}') || (top == '[' && ch == ']') || (top == '(' && ch == ')');
+            }
+        }
+        return stack.isEmpty();
     }
-
 }
