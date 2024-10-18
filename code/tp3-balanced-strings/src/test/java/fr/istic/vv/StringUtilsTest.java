e7b8d4a9536f27db780d6035a51fa89d5f86fbf0
dntelisa <elisabzh10@gmail.com>
2024-10-18 10:41:00 +0200

    Update StringUtilsTest.java

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -6,6 +6,40 @@ import static fr.istic.vv.StringUtils.isBalanced;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
+    @Test
+    void testChaineVide() {
+        assertTrue(isBalanced("")); 
+    }
 
+    @Test
+    void testSymboleImbrique() {
+        assertTrue(isBalanced("{}"));
+        assertTrue(isBalanced("[]"));
+        assertTrue(isBalanced("()"));
+    }
 
+    @Test
+    void testMultiSymboleImbrique() {
+        assertTrue(isBalanced("{}[]"));
+        assertTrue(isBalanced("(){}"));
+    }
+
+    @Test
+    void testMultiImbrique() {
+        assertTrue(isBalanced("{[()]}"));
+        assertTrue(isBalanced("{{}}"));
+    }
+
+    @Test
+    void testSeulFermOuvr() {
+        assertFalse(isBalanced("{[(")); 
+        assertFalse(isBalanced("}])")); 
+    }    
+
+    @Test
+    void testComplexConditions() {
+        assertFalse(isBalanced("{{["));
+        assertFalse(isBalanced("[(])"));
+        assertFalse(isBalanced("{[)}"));
+    }
 }
